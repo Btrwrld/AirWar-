e5dc51f12c310291f066adc4a196fee7da1f285f
Btrwrld <erickm968@gmail.com>
2017-03-21 23:50:01 -0600

    Game framework implementation on OpenGL

---
+++ GameFramework/Game.cpp
@@ -0,0 +1,39 @@
+//
+// Created by erick on 20/03/17.
+//
+
+#include "Game.h"
+#include "ResourceManager.h"
+
+
+Game::Game(GLuint width, GLuint height)
+		: State(GAME_ACTIVE), Keys(), Width(width), Height(height)
+{
+	
+}
+
+Game::~Game()
+{
+	
+}
+
+void Game::Init()
+{
+	
+}
+
+void Game::Update(GLfloat dt)
+{
+	
+}
+
+
+void Game::ProcessInput(GLfloat dt)
+{
+	
+}
+
+void Game::Render()
+{
+	
+}
